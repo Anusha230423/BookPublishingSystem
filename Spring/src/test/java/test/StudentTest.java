bc8a8af3cb5a2f60b33871e6c656c9e98a494230
Anusha <Anusha@Anusha-PC>
2017-06-17 23:13:02 +0530

    test

--- Spring/src/test/java/test/StudentTest.java
+++ Spring/src/test/java/test/StudentTest.java
@@ -1,6 +1,5 @@
 package test;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 
